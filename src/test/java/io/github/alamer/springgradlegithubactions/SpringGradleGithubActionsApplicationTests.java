d51b05c02dcc23a8be2012ca9fd7e49bb8ff97d1
alamer <antilamer87@mail.ru>
2020-04-15 15:48:26 +0800

    initial commit

---
+++ src/test/java/io/github/alamer/springgradlegithubactions/SpringGradleGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alamer.springgradlegithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
