9560a9ef04b9739e937e93b061308e245a781da2
alamer <antilamer87@mail.ru>
2020-04-15 16:10:28 +0800

    add test and assemble tasks

--- src/test/java/io/github/alamer/springgradlegithubactions/SpringGradleGithubActionsApplicationTests.java
+++ src/test/java/io/github/alamer/springgradlegithubactions/SpringGradleGithubActionsApplicationTests.java
@@ -1,5 +1,6 @@
 package io.github.alamer.springgradlegithubactions;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class SpringGradleGithubActionsApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.printf("Test");
+        Assertions.assertTrue(true);
     }
 
 }
